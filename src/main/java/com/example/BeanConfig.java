fbfb92aba9295d7da40e4c86018d690ec2a42568
xDrake <anton.kochurin@gmail.com>
2020-01-15 11:49:44 +0200

    webjars

--- src/main/java/com/example/BeanConfig.java
+++ src/main/java/com/example/BeanConfig.java
@@ -1,18 +1,9 @@
 package com.example;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
-    @Bean
-    public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("api example")
-                        .version("v1")
-                );
-    }
+
 }
--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,11 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
-@Tag(name = "debug")
 @RestController
 @RequestMapping("/debug")
 public class Controller {
-    @Operation(description = "api method example")
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Object debug() {
         return Collections.singletonMap("result", "ok");
